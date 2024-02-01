8039291237d98a0819d7711e9496a48aedbfe393
Vashishth Gajjar <gajjarv2001@gmail.com>
2024-01-31 20:02:18 -0600

    Initial Commit

---
+++ Emoja/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoja
+//
+//  Created by Vashishth gajjar on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoja/EmojaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojaApp.swift
+//  Emoja
+//
+//  Created by Vashishth gajjar on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
